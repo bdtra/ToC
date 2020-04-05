1d67f43fa10eac788edb5e4a5aeb94b6bbc3fda0
Brandon Detra <brandondetra@outlook.com>
2020-04-05 02:04:12 -0500

    folded sections of main class. Added directories.

--- src/main/java/com/gmail/bmdetra/TomesOfCreation.java
+++ src/main/java/com/gmail/bmdetra/TomesOfCreation.java
@@ -1,5 +1,8 @@
+//<editor-fold desc="Package">
 package com.gmail.bmdetra;
+//</editor-fold>
 
+//<editor-fold desc="Imports">
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,42 +18,55 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import java.util.stream.Collectors;
+//</editor-fold>
 
-// The value here should match an entry in the META-INF/mods.toml file
+//<editor-fold desc="The value here should match an entry in the META-INF/mods.toml file">
 @Mod("tomesofcreation")
+//</editor-fold>
+
 public class TomesOfCreation
 {
-    // Directly reference a log4j logger.
+    //<editor-fold desc="Directly reference a log4j logger.">
     private static final Logger LOGGER = LogManager.getLogger();
+    //</editor-fold>
 
+    //<editor-fold desc="Constructor and registers">
     public TomesOfCreation() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
         // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+
         // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
-        // Register ourselves for server and other game events we are interested in
+        // Allows this to be used as an event bus subscriber
         MinecraftForge.EVENT_BUS.register(this);
     }
+    //</editor-fold>
 
+    //<editor-fold desc="setup">
     private void setup(final FMLCommonSetupEvent event)
     {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
+    //</editor-fold>
 
+    //<editor-fold desc="client side stuff">
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
     }
+    //</editor-fold>
 
+    //<editor-fold desc="IMC">
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
@@ -64,13 +80,17 @@ public class TomesOfCreation
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    //</editor-fold>
+
+    //<editor-fold desc="You can use SubscribeEvent and let the Event Bus discover methods to call">
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
+    //</editor-fold>
 
+    //<editor-fold desc="Event Bus Subscriber and Block Registration">
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
@@ -81,4 +101,5 @@ public class TomesOfCreation
             LOGGER.info("HELLO from Register Block");
         }
     }
+    //</editor-fold>
 }
